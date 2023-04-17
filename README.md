15891d3d56447457c541e694663abcced6de29d6
obetdelacruz <pazmichaelandrew70@gmail.com>
2023-04-17 18:48:08 +0800

    Finished structure on html and  Added a Readme file

---
+++ README.md
@@ -0,0 +1,15 @@
+# Calculator
+
+This is a simple Calculator including only the fundamental operations in mathematics (Addition, Subtraction, Multiplication, and Division).
+
+This project has been made by Michael Andrew T. Paz.
+
+Visual Studio Code is used as the source-code editor
+
+Vanilla HTML, CSS, and JavaScript is used to create this.
+
+-- Algorithm used in the planning and creation of this project --
+
+1. Allow the user to input the digits of the 1st terms of the equation
+2. If the user then clicks the desired operation, the screen will remove the inputs to give way for the 2nd part of the equation
+3. If the user then clicks the '=' button, the screen will display the result. Else, if the user clicks any of the operators, assuming they want to go further with the equation, the program will retain the solution from the first two terms then it will operate it to the next desired values and this process will keep on looping until the user finally clicks the '=' which will end the retained values and will display the final solution.
--- script.js
+++ script.js
@@ -1,4 +1,130 @@
-const calculator = (() => {
+const numberScreen = document.querySelector(".number-screen");
+const numberOne = document.getElementById("number-one");
+const numberTwo = document.getElementById("number-two");
+const numberThree = document.getElementById("number-three");
+const numberFour = document.getElementById("number-four");
+const numberFive = document.getElementById("number-five");
+const numberSix = document.getElementById("number-six");
+const numberSeven = document.getElementById("number-seven");
+const numberEight = document.getElementById("number-eight");
+const numberNine = document.getElementById("number-nine");
+const numberZero = document.getElementById("number-zero");
+//Functions
+const functionAdd = document.getElementById("function-add");
+const functionSubtract = document.getElementById("function-subtract");
+const functionMultiply = document.getElementById("function-multiply");
+const functionDivide = document.getElementById("function-divide");
+const functionDecimal = document.getElementById("decimal");
+const functionEquate = document.getElementById("equate");
+
+const screen = [];
+
+function clearInputs() {
+  numberScreen.innerText = "";
+}
+
+//Function Events
+functionAdd.addEventListener("click", function () {
+  numberScreen.innerText += "+";
+  console.log(parseInt(screen.join("")));
+  clearInputs();
+  console.log(screen);
+});
+
+functionSubtract.addEventListener("click", function () {
+  numberScreen.innerText += "-";
+  console.log(parseInt(screen.join("")));
+  clearInputs();
+  console.log(screen);
+});
+
+functionMultiply.addEventListener("click", function () {
+  numberScreen.innerText += "x";
+  console.log(parseInt(screen.join("")));
+  clearInputs();
+  console.log(screen);
+});
+
+functionDivide.addEventListener("click", function () {
+  numberScreen.innerText += "/";
+  console.log(parseInt(screen.join("")));
+  clearInputs();
+  console.log(screen);
+});
+
+functionDecimal.addEventListener("click", function () {
+  numberScreen.innerText += ".";
+  screen.push(".");
+  console.log(screen.join(""));
+  console.log(screen);
+});
+
+functionEquate.addEventListener("click", function () {
+  console.log(screen.join(""));
+});
+
+//Number Events
+numberOne.addEventListener("click", function () {
+  numberScreen.innerText += "1";
+  screen.push(1);
+  console.log(screen);
+});
+
+numberTwo.addEventListener("click", function () {
+  numberScreen.innerText += "2";
+  screen.push(2);
+  console.log(screen);
+});
+
+numberThree.addEventListener("click", function () {
+  numberScreen.innerText += "3";
+  screen.push(3);
+  console.log(screen);
+});
+
+numberFour.addEventListener("click", function () {
+  numberScreen.innerText += "4";
+  screen.push(4);
+  console.log(screen);
+});
+
+numberFive.addEventListener("click", function () {
+  numberScreen.innerText += "5";
+  screen.push(5);
+  console.log(screen);
+});
+
+numberSix.addEventListener("click", function () {
+  numberScreen.innerText += "6";
+  screen.push(6);
+  console.log(screen);
+});
+
+numberSeven.addEventListener("click", function () {
+  numberScreen.innerText += "7";
+  screen.push(7);
+  console.log(screen);
+});
+
+numberEight.addEventListener("click", function () {
+  numberScreen.innerText += "8";
+  screen.push(8);
+  console.log(screen);
+});
+
+numberNine.addEventListener("click", function () {
+  numberScreen.innerText += "9";
+  screen.push(9);
+  console.log(screen);
+});
+
+numberZero.addEventListener("click", function () {
+  numberScreen.innerText += "0";
+  screen.push(0);
+  console.log(screen);
+});
+
+/* const calculator = (() => {
   const add = (a, b) => a + b;
   const subtract = (a, b) => a - b;
   const multiply = (a, b) => a * b;
@@ -8,3 +134,4 @@ const calculator = (() => {
 })();
 
 console.log(calculator.add(2, 4));
+ */
